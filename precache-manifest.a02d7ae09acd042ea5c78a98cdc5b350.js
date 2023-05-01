0dc6eec4bae3c29b844aa642f81e7a1d534731d5
anaisnlawson <anl2140@columbia.edu>
2023-05-01 12:52:05 -0400

    Updates

---
+++ precache-manifest.a02d7ae09acd042ea5c78a98cdc5b350.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad6b3dfe52ef6326d5dc",
+    "url": "/myapp/static/css/main.ae5f9824.chunk.css"
+  },
+  {
+    "revision": "ad6b3dfe52ef6326d5dc",
+    "url": "/myapp/static/js/main.ad6b3dfe.chunk.js"
+  },
+  {
+    "revision": "96834ab56c8198da8a30",
+    "url": "/myapp/static/js/1.96834ab5.chunk.js"
+  },
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "c390c7e1a3bef1defe454d6cb5f2f7b7",
+    "url": "/myapp/static/media/togo_inde.c390c7e1.jpeg"
+  },
+  {
+    "revision": "29b178f98b1f46978e2a757deeb66262",
+    "url": "/myapp/static/media/street.29b178f9.jpeg"
+  },
+  {
+    "revision": "114e048e7966b7579e5eb3904449ff36",
+    "url": "/myapp/static/media/fishing_boat.114e048e.jpeg"
+  },
+  {
+    "revision": "932c22765e63baf7c71cea49486d0518",
+    "url": "/myapp/static/media/kpalime.932c2276.jpeg"
+  },
+  {
+    "revision": "6ad5516df3ab439caac53547336595c5",
+    "url": "/myapp/index.html"
+  }
+];
